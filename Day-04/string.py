04ddb1c56d8febd1aac1863c1097a121f74fd695
MOHD IMRAN <mohdimran@MOHDs-MacBook-Air.local>
2026-07-13 13:27:33 +0530

    Day-04 Strings Part1

---
+++ Day-04/string.py
@@ -0,0 +1,14 @@
+# b = "Hello World"
+# print(b[2:5])
+# print(b[:4])
+# print(b[2:])
+# print(b[-5:-2])
+
+a = "hello World"
+
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(a.replace("H","J"))
+print(a.split(","))
+print(a.capitalize())

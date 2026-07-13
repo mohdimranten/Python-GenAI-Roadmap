58812ebbdfa10af5c0c2ae7770fc898b50167b79
MOHD IMRAN <mohdimran@MOHDs-MacBook-Air.local>
2026-07-13 20:16:41 +0530

    Day-06 grade calculator

---
+++ Day-06/program.py
@@ -0,0 +1,45 @@
+# Voting eligibility
+
+# age = int(input("Enter your age :"))
+
+# if(age >= 18):
+#     print("Congrats! You are eligble for vote")
+# else:
+#     print("Sorry! You are not eligble for vote")    
+
+#Even/Odd
+
+# num = int(input("Enter the num :"))
+
+# if(num % 2==0):
+#     print("Number is even")
+# else:
+#     print("Number is ODD")    
+
+# WAP to Largest number given 3 numbers
+
+# num1 = int(input("Enter the first number :"))
+# num2 = int(input("Enter the second number :"))
+# num3 = int(input("Enter the third number :"))
+
+# if(num1>num2 and num1>num3):
+#     print("The Largest number is :",num1)
+# elif(num2>num1 and num2>num3):
+#     print("The Largest number is :",num2)
+# else:
+#     print("The Largest number is :",num3)    
+# 
+#     
+
+#Grade calculator
+
+# mark = int(input("Enter your mark :"))
+# if(mark>90):
+#     grade = "A"
+# elif(mark>80 and mark<=90):    
+#     grade = "B"
+# elif(mark>70 and mark<=80):
+#     grade = "C"
+# else:
+#     grade = "D"
+# print("Your Grade is :",grade)            

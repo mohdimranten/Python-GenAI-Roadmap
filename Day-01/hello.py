d079d22370c4afc75afc739b4b17b92965968d46
MOHD IMRAN <mohdimran@MOHDs-MacBook-Air.local>
2026-07-13 11:24:31 +0530

    Day1 -Python Basics

--- Day-01/hello.py
+++ Day-01/hello.py
@@ -2,3 +2,19 @@ x = 4
 
 print(x)
 print(type(x))
+
+# my first Comment
+
+"""
+Second type of Comment
+
+"""
+
+x,y,z = "My","Name","MOHD IMRAN"
+print(x,y,z)
+
+l = int(input("'Enter the length value :"))
+w = int(input("Enter the width value :"))
+aot = l*w
+print("The Area of Traingle is : ",aot)
+

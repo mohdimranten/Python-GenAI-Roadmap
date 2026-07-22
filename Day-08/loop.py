028df8a1735a80b65ce88712b014647dbbb30b7f
MOHD IMRAN <mohdimran@MOHDs-MacBook-Air.local>
2026-07-22 17:40:36 +0530

    PyTorch

---
+++ Day-08/loop.py
@@ -0,0 +1,39 @@
+# fruits = ["banana","apple","cherry"]
+
+# for x in fruits:
+    
+#     if(x=="apple"):
+#         break
+#     print(x)  
+
+
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+
+# for x in adj:
+#     for y in fruits:
+#         print(x,y)
+
+
+# for x in range(10):
+#     print(x)
+
+# for x in range(2,30,3):
+#     print(x)
+
+# 1
+# 1 2
+# 1 2 3 
+# 1 2 3 4
+# 1 2 3 4 5
+# row = 5
+# for i in range(1,row+1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()   
+
+row = 5
+for i in range(1,row+1):
+    for j in range(1,i+1):
+        print(j)
+    print()    
